2fdda181e221b6f5b6a1c81dbefa43c6b8251a83
Armando Balche <armando_balche@outlook.com>
2019-05-26 00:00:58 -0500

    Agregada la funcionalidad post prueba 1

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -16,24 +16,43 @@ router.get('/orders/', (req, res) => {
     });
 });
 
-router.post('/orders/add', (req, res) => {
-    var id = req.body.id,
-        status_id = req.body.status_id,
-        customer_id = req.body.customer_id,
-        date = req.body.date,
-        change_log = req.body.change_log;
+// router.get('/orders/add?id:id&status_id:status_id&customer_id:customer_id&date:date&change_log:change_log', (req, res) => {
+//     var id = req.params.id,
+//         status_id = req.params.status_id,
+//         customer_id = req.params.customer_id,
+//         date = req.params.date,
+//         change_log = req.params.change_log;
+//     mysqlConnection.query('INSERT INTO orders VALUES (?,?,?,?,?);',[id,status_id, customer_id, date, change_log], (err, rows, fields) => {
+//         if(!err){
+//             res.send();
+//         } else{
+//             console.log(err);
+//         }
+//     });
+// });
+
+router.get('/orders/add/id=:id&status_id=:status_id&customer_id=:customer_id&date=:date&change_log=:change_log', (req, res) => {
+    var id = req.params.id,
+        status_id = req.params.status_id,
+        customer_id = req.params.customer_id,
+        date = req.params.date,
+        change_log = req.params.change_log;
+
     mysqlConnection.query('INSERT INTO orders values (?,?,?,?,?);',[id,status_id, customer_id, date, change_log], (err, rows, fields) => {
         if(!err){
-            res.send();
+            res.send('agregado exitosamente');
             res.end();
+
         } else{
             console.log(err);
+            console.log('no se pudo agregar');
         }
     });
 });
 
+
 router.delete('/orders/delete:id', (req, res) => {
-    mysqlConnection.query('DELETE FROM orders o WHERE id = ? AND status_id < 2;',[req.params.id], (err, rows, fields) => {
+    mysqlConnection.query('DELETE FROM orders WHERE id =? AND status_id >2',[req.params.id], (err, rows, fields) => {
         if(!err){
             res.send("Borrado exitoso");
             res.end();
